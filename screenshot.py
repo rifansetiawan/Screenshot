ffe281d4a2e7f47a6d9660fbf34051b663f4fd8f
Fernando Christyanto <christyantofernando@gmail.com>
2019-10-03 02:00:22 +0700

    Change maximize windows code to chrome options (fixes maximizing on mac and linux)

--- screenshot.py
+++ screenshot.py
@@ -6,8 +6,16 @@ import os
 
 path = os.getcwd()
 DRIVER = 'chromedriver'
-driver = webdriver.Chrome(DRIVER)
-driver.maximize_window()
+
+chrome_options = webdriver.ChromeOptions()
+if os.name == "nt":
+    # If current OS is Windows
+    chrome_options.add_argument("--start-maximized")
+else:
+    # Other OS (Mac / Linux)
+    chrome_options.add_argument("--kiosk")
+
+driver = webdriver.Chrome(DRIVER, chrome_options = chrome_options)
 driver.get('https://www.findaya.com/')
 time.sleep(10)
 pyautogui.screenshot('image.jpg')
